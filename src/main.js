b275ff78cbd0498f0d07d7c6182c35a482c5c8cd
Spring-_-Bear <springbear2020@163.com>
2022-11-20 05:37:17 +0800

    add the new function of user submit order and pay though the wechat way, also user can view his/her personal order info.

--- src/main.js
+++ src/main.js
@@ -10,12 +10,17 @@ import Pagination from '@/components/Pagination'
 import mockServe from '@/mock/mockServe'
 // 引入 swiper 轮播图样式
 import 'swiper/css/swiper.css'
+// 统一引入 api 接口
+import * as api from '@/api'
+// element-ui 组件按需引入
+import {MessageBox} from 'element-ui'
 
 Vue.config.productionTip = false
 
 Vue.component('TypeNav', TypeNav)
 Vue.component('Carousel', Carousel)
 Vue.component('Pagination', Pagination)
+
 
 new Vue({
     render: h => h(App),
@@ -24,5 +29,10 @@ new Vue({
     beforeCreate() {
         // 装配全局事件总线
         Vue.prototype.$bus = this
+        // 全局 api 对象
+        Vue.prototype.$api = api
+        // element-ui 组件按需引入
+        Vue.prototype.$msgbox = MessageBox;
+        Vue.prototype.$alert = MessageBox.alert;
     }
 }).$mount('#app')
